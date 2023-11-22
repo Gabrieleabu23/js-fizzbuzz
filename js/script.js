1f86a7f0ef151678ca5d293736596818107ab499
Gabrieleabu23 <kaidgabriele23@gmail.com>
2023-11-22 17:26:18 +0100

    feat: style javascript square added class in css

--- js/script.js
+++ js/script.js
@@ -2,18 +2,20 @@
 for(let i=1;i<=100;i++){
     const boxes= document.createElement("square");
     boxes.classList.add("square","d-flex","justify-content-center","align-items-center");
-    boxes.append(i);
     document.querySelector(".row").append(boxes);
     if(i % 15 === 0){
         boxes.innerHTML="fizzbuzz";
-        boxes.style.background="#f0466f";
+        boxes.classList.add("bg_square_15");
     }else if(i % 3 === 0){
         boxes.innerHTML="fizz";
-        boxes.style.background="#0cd6a1";
+        boxes.classList.add("bg_square_3");
     }else if(i % 5 === 0){
         boxes.innerHTML="buzz";
-        boxes.style.background="#ffd166";
+        boxes.classList.add("bg_square_5");
+    }
+    else{
+        boxes.append(i);
     }
     // document.querySelector(".container").append=boxes;
-    console.log(i);
+    // console.log(i);
 }
